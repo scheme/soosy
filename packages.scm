2535eb1aaaadbdd8d96382aa48d0cac9803043b2
Duncan Mak <duncan@a-chinaman.com>
2008-06-23 16:38:04 -0400

    Include SRFI-69 hashtables and SRFI-89 optional arguments.

--- packages.scm
+++ packages.scm
@@ -1,7 +1,7 @@
 ;;; -*- Mode: Scheme; scheme48-package: (config) -*-
 
 (define-structure soosy-helpers soosy-helpers/interface
-  (open scheme define-record-type* signals srfi-1)
+  (open scheme define-record-type* signals srfi-1 srfi-69)
   (for-syntax (open scheme signals srfi-1))
   (files class helpers))
 
@@ -27,3 +27,32 @@
     (export expand-define-record-type*)
   (open scheme destructuring fluids signals receiving)
   (files defrectype))
+
+;;; SRFI 69: Basic hash tables
+;;;
+;;; Implement this using the tables module
+
+(define-interface srfi-69-interface
+  (export make-hash-table hash-table? alist->hash-table
+          ;; hash-table-equivalence-function, hash-table-hash-function
+          hash-table-ref hash-table-ref/default
+          hash-table-set! hash-table-delete! hash-table-exists?
+          ;; hash-table-update! hash-table-update!/default
+          hash-table-size hash-table-keys hash-table-values
+          hash-table-walk ;; hash-table-fold
+          hash-table->alist ;; hash-table-copy hash-table-merge!
+          hash string-hash ;; string-ci-hash hash-by-identity
+          ))
+
+(define-structure srfi-69 srfi-69-interface
+  (open scheme
+        (subset signals (error))
+        define-opt
+        (modify tables (rename (default-hash-function hash))))
+  (files srfi-69))
+
+(define-structure define-opt
+    (export (define* :syntax))
+  (open scheme srfi-1 let-opt)
+  (for-syntax (open scheme let-opt (subset signals (syntax-error)) srfi-1))
+  (files define-opt))
