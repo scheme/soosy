d0add512f356b0adc29e27bed690f2875a0a1f93
Duncan Mak <duncan@prosody.(none)>
2008-06-24 14:09:37 -0400

    Rewrite define-class using simple syntax-rules.

--- macros.scm
+++ macros.scm
@@ -1,30 +1,36 @@
 ;;; -*- Mode: Scheme; scheme48-package: soosy-macros -*-
 
 (define-syntax define-class
-  (lambda (form rename compare)
-    (if (not (and (= (length form) 4)
-                  ;; check class name
-                  (symbol?     (second form))
-                  ;; check superclass
-                  (or (symbol? (third form))
-                      (false?  (third form)))
-                  ;; check variables
-                  (proper-list? (fourth form))))
-        (syntax-error "define-class class-name superclass (var1 var2 ...)")
-        (let* ((name           (second form))
-               (superclass     (third form))
-               (variables      (fourth form))
-               (%define        (rename 'define))
-               (%make-class    (rename 'make-class)))
-          ;; compile-time definition
-          (make-class name (name->class superclass) variables)
-          ;; run-time definition
-          `(,%define ,name (,%make-class ',name ,superclass ',variables))))))
+  (syntax-rules ()
+    ((define-class class-name superclass (inst-vars ...))
+     (define class-name (make-class 'class-name superclass '(inst-vars ...))))))
+
+;; (define-syntax define-class
+;;   (lambda (form rename compare)
+;;     (if (not (and (= (length form) 4)
+;;                   ;; check class name
+;;                   (symbol?     (second form))
+;;                   ;; check superclass
+;;                   (or (symbol? (third form))
+;;                       (false?  (third form)))
+;;                   ;; check variables
+;;                   (proper-list? (fourth form))))
+;;         (syntax-error "define-class class-name superclass (var1 var2 ...)")
+;;         (let* ((name           (second form))
+;;                (superclass     (third form))
+;;                (variables      (fourth form))
+;;                (%define        (rename 'define))
+;;                (%make-class    (rename 'make-class)))
+;;           ;; compile-time definition
+;;           (make-class name (name->class superclass) variables)
+;;           ;; run-time definition
+;;           `(,%define ,name (,%make-class ',name ,superclass ',variables))))))
 
 (define-syntax define-method
   (syntax-rules (lambda)
     ((define-method class (operation object arguments ...) body0 body ...)
-     (class-method-define class 'operation (lambda (object arguments ...) body0 body ...)))
+     (class-method-define class 'operation
+                          (lambda (object arguments ...) body0 body ...)))
     ((define-method class operation method)
      (class-method-define class 'operation method))))
 
